41069c82e36cc23d19cde2fd82f9579158a8872c
Patrycja Petryk <51820512+patrycjapetryk@users.noreply.github.com>
2024-05-21 11:27:45 +0200

    Change navigation links

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 export function Navigation() {
   return (
     <nav className='absolute left-0 top-0 z-50 flex w-full justify-between gap-6 px-6 py-4 text-sm uppercase md:justify-end md:px-10 md:py-6'>
-      <Link className='md:grow' href='#nagrody'>
+      <Link className='md:grow' href='/#nagrody'>
         Nagrody
       </Link>
-      <Link href='#regulamin'>Regulamin</Link>
+      <Link href='/#regulamin'>Regulamin</Link>
       <Link href='https://www.instagram.com/yslbeauty/' target='_blank'>
         Instagram
       </Link>
