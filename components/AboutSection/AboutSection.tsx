c6550fa741b722df3368526c3643b735746a0570
Patrycja Petryk <51820512+patrycjapetryk@users.noreply.github.com>
2024-05-29 18:23:54 +0200

    Change text about competition

--- components/AboutSection/AboutSection.tsx
+++ components/AboutSection/AboutSection.tsx
@@ -4,25 +4,26 @@ export function AboutSection() {
   return (
     <section className='w-full'>
       <Paragraph largeMarginX>
-        Czas wakacji to czas najlepszych koncertów! Przeżyj ekscytującą przygodę
-        i zobacz swoich ulubionych artystów na&nbsp;żywo -&nbsp;pojedź
+        Czas wakacji to&nbsp;czas najlepszych koncertów! Przeżyj ekscytującą
+        przygodę i&nbsp;zobacz swoich ulubionych artystów na żywo -&nbsp;pojedź
         z&nbsp;YSL nad&nbsp;morze na&nbsp;największy w&nbsp;Polsce festiwal
-        muzyczny –&nbsp;Open’er&nbsp;Festiwal&nbsp;2024! Wystarczy,
-        że&nbsp;weźmiesz udział w&nbsp;naszym konkursie.
+        muzyczny&nbsp;-&nbsp;Open’er Festiwal 2024! Wystarczy, że weźmiesz
+        udział w&nbsp;naszym konkursie.
       </Paragraph>
 
       <Paragraph largeText>
-        Na zwyciężczynie czeka aż 10 podwójnych biletów na Open’er Festival
-        na&nbsp;całe 4 dni wydarzenia razem z akomodacją w trakcie wydarzenia
-        oraz WELCOME GIFT od marki YSL BEAUTY. Zabierz swoją bestie
-        i&nbsp;przeżyjcie razem niezapomniany event!
+        Na zwyciężczynie czeka aż&nbsp;10&nbsp;podwójnych biletów
+        na&nbsp;Open’er Festival na&nbsp;całe 4&nbsp;dni wydarzenia razem
+        z&nbsp;akomodacją w trakcie wydarzenia oraz WELCOME GIFT od marki YSL
+        BEAUTY. Zabierz swoją bestie i&nbsp;przeżyjcie razem niezapomniany
+        event!
       </Paragraph>
 
       <Paragraph largeText>
         Jak wziąć udział? Zobacz TikToki dodane przez @tojahania oraz
-        @majafirekk i&nbsp;odpowiedz w komentarzu na pytanie: „Bez którego z
-        produktów YSL nie wyobrażasz sobie festiwalowego makijażu i odpowiedz,
-        dlaczego właśnie Ty chciałabyś pojechać na festiwal razem z YSL BEAUTY?”
+        @majafirekk i&nbsp;odpowiedz w&nbsp;komentarzu na&nbsp;pytanie: „Jakich
+        kosmetyków YSL Beauty użyjesz do&nbsp;stworzenia festiwalowego makijażu
+        i&nbsp;dlaczego?”
       </Paragraph>
 
       <Paragraph>Nie zapomnij oznaczyć w komentarzu:</Paragraph>
